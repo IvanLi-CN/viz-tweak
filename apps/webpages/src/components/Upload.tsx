9636cc6d900a10a827848e65b40d89cbeaf853cf
Ivan Li <ivanli2048@gmail.com>
2024-12-04 20:30:56 +0800

    feat: add drag-and-drop upload support
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- apps/webpages/src/components/Upload.tsx
+++ apps/webpages/src/components/Upload.tsx
@@ -1,13 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
+import axios from "axios";
 import clsx from "clsx";
 import ky from "ky";
 import pLimit from "p-limit";
 import {
   type ChangeEventHandler,
+  type DragEventHandler,
   type FC,
   type MouseEventHandler,
   useCallback,
+  useEffect,
+  useRef,
   useState,
 } from "react";
 import { type Chunk, getChunks } from "../helpers/file.ts";
@@ -18,19 +22,43 @@ type ChunkStatus = "pending" | "done" | "error";
 const Upload: FC = () => {
   const navigate = useNavigate();
 
-  const [_chunks, setChunks] = useState<(Chunk & { status: ChunkStatus })[]>([]);
+  const [chunks, setChunks] = useState<
+    (Chunk & { status: ChunkStatus; progress: number })[]
+  >([]);
+  const [blobUrl, setBlobUrl] = useState<string>();
+  const [file, setFile] = useState<File>();
+  const [imageDimensions, setImageDimensions] = useState<{
+    width: number;
+    height: number;
+  }>();
+  const dropRef = useRef<HTMLLabelElement>(null);
 
-  const updateChunk = useCallback((number: number, status: ChunkStatus) => {
-    setChunks((prev) =>
-      prev.map((c) => {
-        if (c.part === number) {
-          return { ...c, status };
-        }
+  const updateChunk = useCallback(
+    (number: number, status: ChunkStatus, progress?: number) => {
+      setChunks((prev) =>
+        prev.map((c) => {
+          if (c.part === number) {
+            return { ...c, status, progress: progress ?? c.progress };
+          }
 
-        return c;
-      }),
-    );
-  }, []);
+          return c;
+        }),
+      );
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setBlobUrl(url);
+      createAttachment(file).catch(console.error);
+
+      return () => {
+        URL.revokeObjectURL(url);
+      };
+    }
+  }, [file]);
 
   const {
     mutateAsync: createAttachment,
@@ -56,6 +84,7 @@ const Upload: FC = () => {
       const chunks = getChunks(file).map((c) => ({
         ...c,
         status: "pending" as ChunkStatus,
+        progress: 0,
       }));
       setChunks(chunks);
 
@@ -65,25 +94,49 @@ const Upload: FC = () => {
           uploadId,
           partNumber: part,
         });
+        updateChunk(part, "pending", 0);
 
-        const etag = await ky(url, {
-          method: "PUT",
-          body: blob,
-          headers: {
-            "Content-Type": "application/octet-stream",
-          },
-          retry: {
-            limit: 3,
-          },
-        }).then((req) => {
-          return req.headers.get("etag") ?? undefined;
-        });
+        const etag = await axios
+          .put(url, blob, {
+            headers: {
+              "Content-Type": "application/octet-stream",
+            },
+            onUploadProgress: (ev) => {
+              updateChunk(part, "pending", ev.total ? ev.loaded / ev.total : 0);
+            },
+          })
+          .then((req) => {
+            return req.headers.etag ?? undefined;
+          });
 
-        updateChunk(part, "done");
+        updateChunk(part, "done", 1);
 
         return { part, etag };
       };
-      const limit = pLimit(1);
+      const limit = pLimit(2);
+
+      // await Promise.all(
+      //   chunks.map((c, i) =>
+      //     limit(
+      //       () =>
+      //         new Promise<void>((resolve, reject) => {
+      //           let p = 0;
+      //           const intervalId = setInterval(() => {
+      //             p += 0.003;
+      //             updateChunk(c.part, "pending", p);
+      //           }, 100);
+      //           setTimeout(() => {
+      //             clearInterval(intervalId);
+      //             if (i === chunks.length - 1) {
+      //               reject(new Error("All chunks uploaded, but mock error"));
+      //             } else {
+      //               resolve();
+      //             }
+      //           }, 30 * 1000);
+      //         }),
+      //     ),
+      //   ),
+      // );
 
       const parts = await Promise.all(
         chunks.map((c) => limit(() => uploadChunk(c))),
@@ -106,6 +159,7 @@ const Upload: FC = () => {
 
   const handleClick: MouseEventHandler<HTMLInputElement> = (ev) => {
     ev.currentTarget.value = "";
+    setFile(undefined);
   };
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (ev) => {
@@ -119,18 +173,119 @@ const Upload: FC = () => {
       return;
     }
 
-    createAttachment(file);
+    setFile(file);
+  };
+
+  const handleDrop: DragEventHandler<HTMLElement> = (ev) => {
+    ev.preventDefault();
+
+    const files = Array.from(ev.dataTransfer.files);
+
+    if (!files.length) {
+      return;
+    }
+
+    const file = files[0];
+
+    if (!file) {
+      return;
+    }
+
+    setFile(file);
+  };
+
+  const handleDragOver: DragEventHandler<HTMLElement> = (ev) => {
+    ev.preventDefault();
+  };
+
+  const handleDragLeave: DragEventHandler<HTMLElement> = () => {
+    dropRef.current?.classList.remove("bg-primary/20");
+  };
+
+  const handleDragEnter: DragEventHandler<HTMLElement> = (ev) => {
+    console.log(ev);
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "copy";
+    ev.stopPropagation();
+    dropRef.current?.classList.add("bg-primary/20");
   };
 
   return (
-    <div
+    <label
       className={clsx(
         "border-2 border-dashed border-base-content/50 rounded-box",
         "hover:cursor-pointer hover:border-primary/80 hover:shadow-lg hover:text-primary",
         "active:border-primary active:bg-primary/10",
         "transition-colors",
+        "max-w-6xl mx-auto aspect-video",
+        "flex flex-col items-center justify-evenly",
+        "relative",
       )}
+      ref={dropRef}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDragEnter={handleDragEnter}
+      onDrop={handleDrop}
     >
+      {blobUrl && isPending && (
+        <div className="p-4 absolute top-0 left-0 w-full h-full text-center">
+          <img
+            src={blobUrl}
+            alt="Preview"
+            className="mx-auto h-full w-full object-contain"
+            onLoad={(ev) => {
+              const imageEl = ev.currentTarget as HTMLImageElement;
+
+              if (imageEl.complete) {
+                const { naturalWidth, naturalHeight, width, height } = imageEl;
+
+                const ratio = naturalWidth / naturalHeight;
+
+                if (width / height > ratio) {
+                  setImageDimensions({
+                    width: Math.ceil(height * ratio),
+                    height: height,
+                  });
+                } else {
+                  setImageDimensions({
+                    width: width,
+                    height: Math.ceil(width / ratio),
+                  });
+                }
+              }
+            }}
+          />
+          <ol
+            className="grid grid-cols-1 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute"
+            style={{
+              height: `${imageDimensions?.height ?? 100}px`,
+              width: `${imageDimensions?.width ?? 100}px`,
+            }}
+          >
+            {chunks.map((c) => (
+              <li
+                key={c.part}
+                className={clsx(
+                  "backdrop-brightness-50 justify-self-end backdrop-blur-sm transition-all duration-500",
+                )}
+                style={{
+                  width: `${(() => {
+                    switch (c.status) {
+                      case "pending":
+                        return (1 - c.progress) * 100;
+                      case "done":
+                        return 0;
+                      default:
+                        return 100;
+                    }
+                  })()}%`,
+                }}
+              />
+            ))}
+          </ol>
+        </div>
+      )}
+
       {error && (
         <div className="p-4">
           <div>{error.message}</div>
@@ -142,15 +297,9 @@ const Upload: FC = () => {
         </div>
       )}
       {isIdle && (
-        <label className="p-4 flex justify-center items-center min-h-96">
+        <div className="p-4 flex justify-center items-center">
           <div>{isPending ? "Uploading..." : "Upload"}</div>
-          <input
-            type="file"
-            hidden
-            onChange={handleChange}
-            onClickCapture={handleClick}
-          />
-        </label>
+        </div>
       )}
       {isPending && (
         <div className="p-4 flex justify-center items-center min-h-96">
@@ -162,7 +311,13 @@ const Upload: FC = () => {
           <div>Uploaded</div>
         </div>
       )}
-    </div>
+      <input
+        type="file"
+        hidden
+        onChange={handleChange}
+        onClickCapture={handleClick}
+      />
+    </label>
   );
 };
 
